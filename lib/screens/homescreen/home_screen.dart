1685f9732721a0c349136bfabf2faa255befa1b3
akzholshakterbekuly <akzhol1221@gmail.com>
2023-06-12 22:11:42 +0600

    + small fixes

--- lib/screens/homescreen/home_screen.dart
+++ lib/screens/homescreen/home_screen.dart
@@ -1,259 +1,3 @@
-// import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import "package:flutter/material.dart";
-// import 'package:freelance_app/screens/activity/activity.dart';
-// import 'package:freelance_app/screens/homescreen/clients_components/event.dart';
-
-// import 'package:freelance_app/screens/profile/profile.dart';
-// import 'package:freelance_app/screens/projects_jobs/architects_projects/projects.dart';
-// import 'package:freelance_app/utils/colors.dart';
-// import 'package:freelance_app/screens/homescreen/components/posted_events.dart';
-// import 'package:freelance_app/screens/homescreen/sidebar.dart';
-// import 'package:freelance_app/screens/projects_jobs/architects_projects/project_decription.dart';
-
-// class HomeScreen extends StatefulWidget {
-//   const HomeScreen({super.key});
-
-//   @override
-//   State<HomeScreen> createState() => _HomeScreenState();
-// }
-
-// class _HomeScreenState extends State<HomeScreen> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: BottomNavigationPage(
-//         title: "ArchNEO",
-//       ),
-//     );
-//   }
-// }
-
-// class BottomNavigationPage extends StatefulWidget {
-//   const BottomNavigationPage({Key? key, required this.title}) : super(key: key);
-//   final String title;
-
-//   @override
-//   _BottomNavigationPageState createState() => _BottomNavigationPageState();
-// }
-
-// class _BottomNavigationPageState extends State<BottomNavigationPage> {
-//   late int currentIndex;
-//   User? user = FirebaseAuth.instance.currentUser;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     currentIndex = 0;
-//   }
-
-//   void changePage(int? index) {
-//     setState(() {
-//       currentIndex = index!;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final _uid = user!.uid;
-//     final _email = user!.email;
-//     return Scaffold(
-//       body: <Widget>[
-//         const Homepage(),
-//         const Projects(),
-//         const Activity(),
-//         ProfilePage(
-//           userID: _uid,
-//           uEmail: _email,
-//         ),
-//       ][currentIndex],
-//       floatingActionButton:
-//           // ignore: unrelated_type_equality_checks
-//           currentIndex == 0
-//               ? FloatingActionButton(
-//                   backgroundColor: const Color(0xffD2A244),
-//                   onPressed: () {
-//                     Navigator.push(
-//                       context,
-//                       MaterialPageRoute(
-//                           builder: (_) => Upload(
-//                                 userID: _uid,
-//                               ) //const LoginScreen(),
-//                           ),
-//                     );
-//                   },
-//                   child: const Icon(
-//                     Icons.add_rounded,
-//                     //size: 40,
-//                     color: Colors.white,
-//                   ),
-//                 )
-//               : currentIndex == 3
-//                   //button for project making
-//                   ? FloatingActionButton(
-//                       backgroundColor: const Color(0xffD2A244),
-//                       onPressed: () {
-//                         Navigator.push(
-//                           context,
-//                           MaterialPageRoute(
-//                               builder: (_) => ProjUpload(
-//                                     userID: _uid,
-//                                     uEmail: _email,
-//                                   ) //const LoginScreen(),
-//                               ),
-//                         );
-//                       },
-//                       child: const Icon(
-//                         Icons.add_rounded,
-//                         //size: 40,
-//                         color: Colors.white,
-//                       ),
-//                     )
-//                   : null,
-//       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-//       bottomNavigationBar: BubbleBottomBar(
-//         backgroundColor: Colors.white,
-//         hasNotch: true,
-//         // fabLocation: BubbleBottomBarFabLocation.end,
-//         opacity: 1,
-//         currentIndex: currentIndex,
-//         onTap: changePage,
-//         borderRadius: const BorderRadius.vertical(
-//           top: Radius.circular(0),
-//         ), //border radius doesn't work when the notch is enabled.
-//         //elevation: 10,
-//         tilesPadding: const EdgeInsets.symmetric(
-//           vertical: 8.0,
-//         ),
-
-//         items: const <BubbleBottomBarItem>[
-//           BubbleBottomBarItem(
-//             backgroundColor: Color.fromARGB(255, 14, 14, 54),
-//             icon: const Icon(
-//               Icons.dashboard,
-//               color: Colors.black,
-//             ),
-//             activeIcon: Icon(
-//               Icons.dashboard,
-//               color: Colors.white,
-//             ),
-//             title: Text(
-//               "Home ",
-//               style: TextStyle(color: Colors.white, fontSize: 16),
-//             ),
-//           ),
-//           BubbleBottomBarItem(
-//               backgroundColor: Color.fromARGB(255, 14, 14, 54),
-//               icon: const Icon(
-//                 Icons.search,
-//                 color: Colors.black,
-//               ),
-//               activeIcon: Icon(
-//                 Icons.search,
-//                 color: Colors.white,
-//               ),
-//               title: Text(
-//                 "Projects",
-//                 style: TextStyle(color: Colors.white, fontSize: 16),
-//               )),
-//           BubbleBottomBarItem(
-//               backgroundColor: Color.fromARGB(255, 14, 14, 54),
-//               icon: const Icon(
-//                 Icons.library_books,
-//                 color: Colors.black,
-//               ),
-//               activeIcon: Icon(
-//                 Icons.library_books,
-//                 color: Colors.white,
-//               ),
-//               title: Text(
-//                 "Events",
-//                 style: TextStyle(color: Colors.white, fontSize: 16),
-//               )),
-//           BubbleBottomBarItem(
-//               backgroundColor: Color.fromARGB(255, 14, 14, 54),
-//               icon: const Icon(
-//                 Icons.person_outline,
-//                 color: Colors.black,
-//               ),
-//               activeIcon: Icon(
-//                 Icons.person_outline_rounded,
-//                 color: Colors.white,
-//               ),
-//               title: Text(
-//                 "Profile",
-//                 style: TextStyle(color: Colors.white, fontSize: 16),
-//               )),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// class Homepage extends StatefulWidget {
-//   const Homepage({super.key});
-
-//   @override
-//   State<Homepage> createState() => _HomepageState();
-// }
-
-// class _HomepageState extends State<Homepage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       drawer: SideBar(),
-//       appBar: AppBar(
-//         elevation: 0,
-//         backgroundColor: white,
-//         iconTheme: const IconThemeData(
-//           color: Color(0xffD2A244),
-//         ),
-//         title: const Padding(
-//           padding: EdgeInsets.only(left: 180),
-//           child: Text(
-//             "ArchNEO",
-//             style: TextStyle(color: Color(0xffD2A244)),
-//           ),
-//         ),
-//       ),
-//       body: SafeArea(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.start,
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: const [
-//             Padding(
-//               padding: EdgeInsets.only(left: 15, top: 20),
-//               child: Text(
-//                 "Join Upcoming ",
-//                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-//               ),
-//             ),
-//             Padding(
-//               padding: EdgeInsets.only(left: 15, bottom: 15),
-//               child: Text(
-//                 "Events",
-//                 style: TextStyle(
-//                   color: Color(0xffD2A244),
-//                   fontWeight: FontWeight.w900,
-//                   fontSize: 30,
-//                 ),
-//               ),
-//             ),
-//             // Category(),
-//             SizedBox(
-//               height: 10,
-//             ),
-//             Events(),
-
-//             //Bottomnavbar(),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -280,6 +24,24 @@ class _HomeScreenState extends State<HomeScreen> {
   String collectionName = '';
   String profileID = '';
   final auth = FirebaseAuth.instance;
+
+  void getMyData() async {
+    final QuerySnapshot rolesDoc = await FirebaseFirestore.instance
+        .collection('roles')
+        .where('Email', isEqualTo: auth.currentUser!.email)
+        .get();
+
+    setState(() {
+      collectionName = rolesDoc.docs[0]['Role'] + 's';
+      profileID = rolesDoc.docs[0]['ID'];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getMyData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -336,20 +98,12 @@ class _BottomNavigationPageState extends State<BottomNavigationPage> {
           collectionName: widget.collectionName,
           profileID: widget.profileID,
         ),
-        Projects(
-            // userID: _uid,
-            // collectionName: widget.collectionName,
-            // profileID: widget.profileID,
-            ),
+        Projects(),
         const Activity(),
-        // userID: _uid,
-        // collectionName: widget.collectionName,
-        // profileID: widget.profileID,
         ProfilePage(
           userID: _uid,
-          // collectionName: widget.collectionName,
-          // profileID: widget.profileID,
-          uEmail: _email,
+          collectionName: widget.collectionName,
+          profileID: widget.profileID,
         ),
       ][currentIndex],
       floatingActionButton: currentIndex == 0
